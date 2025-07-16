eb9fa1cbd7addcb9cdfa6e80e2fbbc74d4b7861f
Daniel Lopez <dany5seg@gmail.com>
2025-07-16 17:09:31 -0600

    Copia de seguridad

--- app/controllers/joyasController.js
+++ app/controllers/joyasController.js
@@ -13,6 +13,46 @@ function buscarTodos(req, res) {
         })
 }
 
+function agregarJoya(req, res) {
+    new joyasModel(req.body).save()
+        .then(info => {
+            return res.status(200).send({
+                message: 'La Informacion se guarda de manera correcta',
+                info
+            })
+        })
+        .catch(e => {
+            return res.status(404).send({message: 'Error al guardar la informacion'})
+        })
+}
+
+function buscarJoya(req, res, next) {
+    var consulta = {}
+    consulta[req.params.key]=req.params.value
+    joyasModel.find(consulta)
+       .then(joyas => {
+            if (!joyas.length) return next();
+            req.body.joyas = joyas
+            return next();
+        })
+      .catch(e => {
+            req.body.e = e
+            return next();
+      });
+}
+
+function mostrarJoya(req, res) {
+    if (req.body.e) return res.status(404).send({message: 'Error al consultar la informacion'})
+    if (req.body.joyas) return res.status(204).send({message: 'No hay nada que mostrar'})
+    let joyas = req.body.joyas
+    return res.status(200).send({joyas})
+}
+
+
+
 module.exports = {
-    buscarTodos
+    buscarTodos,
+    agregarJoya,
+    buscarJoya,
+    mostrarJoya
 }
--- app/routes/joyasRoute.js
+++ app/routes/joyasRoute.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const joyasController = require('../controllers/joyasController')
 
 router.get('/', joyasController.buscarTodos)
+    .post('/', joyasController.agregarJoya)
+    .get('/:key/:value', joyasController.buscarJoya),joyasController.mostrarJoya
 
 
 module.exports = router
